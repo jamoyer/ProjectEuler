970d709bd5d9b962825a8da3a9ee28c9079dfc27
Jacob Moyer <jamoyer@iastate.edu>
2017-11-15 20:01:38 -0800

    Simplified problem 44 a bit.

--- src/Problem44PentagonNumbers.java
+++ src/Problem44PentagonNumbers.java
@@ -1,31 +1,25 @@
-import java.util.Collections;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
 
 public class Problem44PentagonNumbers {
 
     public static void main(String[] args) {
-        final Set<Long> pentagonals = new LinkedHashSet<>();
+        final Set<Long> pentagonals = new HashSet<>();
 
         for (long i = 1; ; i++) {
             final long pentagon = i * (3 * i - 1) / 2;
             pentagonals.add(pentagon);
-            final Optional<Long> pentagonalNum = findPentagonalSumAndDifference(pentagon, pentagonals);
+            final Optional<Long> pentagonalNum = pentagonals.stream()
+                    .filter(num -> pentagonals.contains(pentagon - num))
+                    .map(num -> num * 2 - pentagon)
+                    .filter(pentagonals::contains)
+                    .findFirst();
             if (pentagonalNum.isPresent()) {
                 System.out.println(pentagonalNum.get());
                 break;
             }
         }
-    }
-
-    private static Optional<Long> findPentagonalSumAndDifference(final long pentagonal, final Set<Long> pentagonals) {
-        return pentagonals.stream()
-                .sorted(Collections.reverseOrder())
-                .filter(num -> pentagonals.contains(pentagonal - num))
-                .map(num -> num * 2 - pentagonal)
-                .filter(pentagonals::contains)
-                .findFirst();
     }
 
 }
